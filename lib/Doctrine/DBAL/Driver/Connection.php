a07bba6611584744bcf8f8fe475f93824a7f4aad
Muhammad Mahdi Husayni <mmhazizan@silversky.com>
2023-07-10 09:45:42 +0800

    try \PDOStatement

--- lib/Doctrine/DBAL/Driver/Connection.php
+++ lib/Doctrine/DBAL/Driver/Connection.php
@@ -36,7 +36,7 @@ interface Connection
      *
      * @return \Doctrine\DBAL\Driver\Statement
      */
-    function prepare($statement, array $options = []): PDOStatement|false;
+    function prepare($statement, array $options = []): \PDOStatement|false;
 
     /**
      * Executes an SQL statement, returning a result set as a Statement object.
